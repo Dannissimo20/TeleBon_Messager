896d76154d3fb7756addbc3c873ab88e1035dd98
Dannissimo20 <Denicha_d_89@mail.ru>
2023-12-14 16:59:27 +0300

    Create_message function

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ from pydantic import BaseModel, Field
 app = FastAPI()
 engine = create_engine('postgresql+psycopg2://postgres:denchik2702@localhost:5432/telebon')
 
+conn = engine.connect()
+
 metadata = MetaData()
 
 chats = Table('chat', metadata,
               Column('id', String, primary_key=True),
               Column('chatName', String),
               Column('isGroupChat', Boolean),
+              Column('latestMessage', String, ForeignKey('message.id')),
+              Column('groupAdmin', String),
               Column('createdAt', DateTime),
               Column('updatedAt', DateTime))
 
@@ -22,10 +26,26 @@ chatUsers = Table('chatUsers', metadata,
                   Column('user', String),
                   Column('chat', String))
 
+messages = Table('message', metadata,
+                 Column('id', String, primary_key=True),
+                 Column('readBy', String),
+                 Column('sender', String),
+                 Column('content', String),
+                 Column('chat', String, ForeignKey('chat.id')),
+                 Column('createdAt', DateTime),
+                 Column('updatedAt', DateTime))
+
 class UserModel(BaseModel):
     email: str = Field(..., description="Электронная почта")
     name: str = Field(..., description="ФИО")
     id: str = Field(..., description="id пользователя")
+
+class ChatModel(BaseModel):
+    id: str = Field(..., description="id чата")
+    chatName: str = Field(..., description="Название чата")
+    isGroupChat: bool = Field(default=False, description="Является ли чат групповым")
+    createdAt: datetime = Field(default=datetime.now(), description="Время создания чата")
+    updatedAt: datetime = Field(default=datetime.now(), description="Время обновления чата")
 
 
 class CreateChatModel(BaseModel):
@@ -34,6 +54,14 @@ class CreateChatModel(BaseModel):
     createdAt: datetime = Field(default=datetime.now(), description="Время создания чата")
     updatedAt: datetime = Field(default=datetime.now(), description="Время обновления чата")
     users: list[UserModel]
+
+class CreateMessageModel(BaseModel):
+    readBy: list[UserModel] = Field(default=None, description="Пользователь/ли, который прочитал сообщение")
+    sender: UserModel = Field(..., description="Отправитель сообщения")
+    content: str = Field(..., description="Текст сообщения")
+    chat: ChatModel
+    createdAt: datetime = Field(default=datetime.now(), description="Время создания сообщения")
+    updatedAt: datetime = Field(default=datetime.now(), description="Время обновления сообщения")
 
 
 
@@ -57,13 +85,36 @@ async def create_chat(chat: CreateChatModel):
         createdAt=chat.createdAt,
         updatedAt=chat.updatedAt,
     )
-    conn = engine.connect()
     conn.execute(chat_insert)
-    for user in chat.users:
+    for item in chat.users:
         chatUsers_insert = chatUsers.insert().values(
-            user=user.id,
+            user=item.id,
             chat=str(uniq_id)
         )
         conn.execute(chatUsers_insert)
+    conn.commit()
+    return {"code": 200}
+
+@app.post("/create_meassage")
+async def create_meassage(message_model: CreateMessageModel):
+    uniq_id = uuid.uuid4()
+    if(len(message_model.readBy) == 0):
+        readBy = None
+    else:
+        readBy = message_model.readBy[0].id
+    message_insert = insert(messages).values(
+        id=uniq_id,
+        readBy=readBy,
+        sender=message_model.sender.id,
+        content=message_model.content,
+        chat=message_model.chat.id,
+        createdAt=message_model.createdAt,
+        updatedAt=message_model.updatedAt,
+    )
+    conn.execute(message_insert)
+    chat_update = update(chats).where(chats.c.id == message_model.chat.id).values(
+        latestMessage=uniq_id,
+    )
+    conn.execute(chat_update)
     conn.commit()
     return {"code": 200}
